4094d845e26cd8f4cd8d93e1a0052c4af28bba16
yzm <13884935@qq.com>
2024-05-13 11:17:34 +0900

    all

---
+++ packages/server/src/routes/marketplaces/index.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+import marketplacesController from '../../controllers/marketplaces'
+import marketplacesController from '../../controllers/marketplaces'
+const router = express.Router()
+
+// READ
+router.get('/templates', marketplacesController.getAllTemplates)
+
+export default router
